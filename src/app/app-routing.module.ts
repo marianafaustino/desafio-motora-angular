0f82766dc45a4ca7f0b50792dcb28ae30eca5e45
mariana faustino <marifaustino@gmail.com>
2025-02-11 09:49:44 -0300

    feat: criar modulo dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'viagens', loadChildren: () => import('./viagens/viagens.module').then(m => m.ViagensModule) },
   { path: 'motoristas', loadChildren: () => import('./motoristas/motoristas.module').then(m => m.MotoristasModule) },
   { path: 'veiculos', loadChildren: () => import('./veiculos/veiculos.module').then(m => m.VeiculosModule) },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
