a0a84aba6fde439ccf3f025370fc829e540916f9
mariana-dmtech <mariana@dmtech.ltd>
2025-02-14 14:44:38 -0300

    feat: condicao para exibir divisor e botao add somente em rotas selecionadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { NovoItemDialogComponent } from './novo-item-dialog/novo-item-dialog.component';
 import { Subject } from 'rxjs';
 
@@ -12,22 +12,29 @@ import { Subject } from 'rxjs';
 })
 export class AppComponent {
   title = 'Driver Analytics';
-
   novoItemAdicionado = new Subject<void>();
+  exibirBotoes = false;
 
-  constructor(private dialog: MatDialog, private router: Router) {}
+  constructor(private dialog: MatDialog, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const rotasPermitidas = ['/viagens', '/motoristas', '/veiculos'];
+        this.exibirBotoes = rotasPermitidas.includes(this.router.url); 
+      }
+    });
+  }
 
   openDialog(): void {
     let title = '';
     let entity = '';
 
-    if (this.router.url.includes('/motoristas')) {
+    if (this.router.url === '/motoristas') {
       title = 'Novo Motorista';
       entity = 'motorista';
-    } else if (this.router.url.includes('/viagens')) {
+    } else if (this.router.url === '/viagens') {
       title = 'Nova Viagem';
       entity = 'viagem';
-    } else if (this.router.url.includes('/veiculos')) {
+    } else if (this.router.url === '/veiculos') {
       title = 'Novo Veículo';
       entity = 'veículo';
     }
